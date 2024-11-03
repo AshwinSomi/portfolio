a8e3cc66b005a650ac51053cb991a9353b31374f
Ashwin065 <ashwinsomi@gmail.com>
2024-11-03 17:59:17 -0500

    update-1

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import pic from "../assets/pic2.png";
 import Projects from "./Projects";
 import { FaGithub } from "react-icons/fa";
 import { LiaGithubSquare } from "react-icons/lia";
+import { IoTriangleSharp } from "react-icons/io5";
+import { RxTriangleRight } from "react-icons/rx";
+import PlayGame from "./PlayGame";
 
 export default function Body() {
+  const resumeLink = "";
   return (
     <div id="top" className="p-6 text-white ">
       <div>
@@ -18,16 +22,24 @@ export default function Body() {
           <p>Interested in learning and developing new things.</p>
           <p>
             You can connect me at{" "}
-            <a className="underline " href="ashwinsomi@gmail.com">
+            <a className="underline " href="mailto:ashwinsomi@gmail.com">
               <span>ashwinsomi@gmail.com</span>
             </a>
           </p>
         </div>
-        <img
-          src={pic}
-          alt="Picture"
-          className="h-52 w-auto px-6 py-auto  "
-        ></img>
+        <div className="relative group ">
+          <img
+            src={pic}
+            alt="Picture"
+            className="h-36 md:h-44 lg:h-52 w-auto mx-auto px-6 py-auto transition-all duration-300 ease-in-out opacity-90 hover:opacity-100 hover:scale-105 "
+          ></img>
+          {/* <div
+            className="absolute picBlock bg-gray-300 
+                      opacity-100 
+                      group-hover:opacity-0
+                      transition-opacity duration-500"
+          ></div> */}
+        </div>
       </div>
       <div className="text-xs md:text-sm lg:text-base  ">
         <p className="py-6 ">
@@ -60,12 +72,43 @@ export default function Body() {
         </p>
         <Projects />
       </div>
-      <div id="resume" className="text-xs md:text-sm lg:text-base  ">
+      <div id="futureGoals" className="text-xs md:text-sm lg:text-base  ">
         <p className="py-6 ">
-          <span>RESUME</span>
+          <span>FUTURE GOALS</span>
         </p>
-        <div>...resume...</div>
+        <div>
+          <p>
+            -Learn ML models and try to implement in a project
+            <br />
+            -Create Full Stack project focussing in backend utilizing
+            distributed systems
+          </p>
+        </div>
       </div>
+      <div id="resume" className="text-xs md:text-sm lg:text-base  ">
+        <details className="py-6 flex items-center cursor-pointer ">
+          <summary>
+            <span>RESUME</span>
+          </summary>
+          {resumeLink === "" ? (
+            <p>- it seems link is not setup yet</p>
+          ) : (
+            <a href={resumeLink} className="underline ">
+              resume link
+            </a>
+          )}
+        </details>
+      </div>
+
+      <div id="playGame" className="text-xs md:text-sm lg:text-base  ">
+        <details className="py-6 flex items-center cursor-pointer ">
+          <summary>
+            <span>PLAY A GAME</span>
+          </summary>
+          <PlayGame />
+        </details>
+      </div>
+
       <div id="media" className="text-xs md:text-sm lg:text-base  ">
         <p className="py-6 ">
           <span>MEDIA</span>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import picTrackDaily from "../assets/track-daily-main.png";
 
 export default function Projects() {
   return (
@@ -6,50 +7,77 @@ export default function Projects() {
       <table className=" p-4 ">
         <tbody>
           <tr>
-            <td className={"border border-2 p-4 py-4 "}>TrackDaily</td>
+            <td className={"border border-2 p-2 py-4 "}>
+              <a
+                href="https://ashwin065-track-daily.netlify.app/"
+                target="blank"
+              >
+                Track Daily
+              </a>
+            </td>
             <td className="border border-2 p-4 py-4 ">
               <p className="py-3">
-                Lets you track you mood and wite you thoughts for that day
+                - Lets you track you mood and wite you thoughts for that day
                 through out the month, year, ....
               </p>
               <p className="py-3">
-                Used firebase to provide Authentication and store data.{" "}
+                - Used firebase to provide Authentication and store data.{" "}
               </p>
+              <a
+                href="https://ashwin065-track-daily.netlify.app/"
+                target="blank"
+              >
+                <img
+                  src={picTrackDaily}
+                  alt="web app pic"
+                  className="object-center h-36 md:h-44 lg:h-52 w-auto p-4 mx-auto 
+                transition-all duration-500 ease-in-out opacity-90 hover:opacity-110 hover:scale-110 "
+                ></img>
+              </a>
               <p className="py-3 ">
                 Tech stack: Next.js, Tailwind CSS, JavaScript, Firebase
               </p>
               <a
+                href="https://ashwin065-track-daily.netlify.app/"
+                className="underline py-3 font_tiny"
+                target="blank"
+              >
+                track-daily
+              </a>{" "}
+              <a
                 href="https://github.com/AshwinSomi/track_daily-next-app"
                 className="underline py-3 font_tiny"
+                target="blank"
               >
                 github
               </a>
             </td>
           </tr>
           <tr>
-            <td className="border border-2 p-4 py-4 ">QuizApp</td>
+            <td className="border border-2 p-2 py-4 ">Quiz App</td>
             <td className="border border-2 p-4 py-4 ">
               <p className="py-3">
-                Lets you take a quiz of set of 10 questions.
+                - Lets you take a quiz of set of 10 questions.
               </p>
               <p className="py-3">Tech stack: React.js, CSS, JavaScript</p>
               <a
                 href="https://github.com/AshwinSomi/React_quizApplication_1/tree/main/quiz_app_1"
                 className="underline py-3 font_tiny "
+                target="blank"
               >
                 github
               </a>
             </td>
           </tr>
           <tr>
-            <td className={"border border-2 p-4 py-4 "}>NewsAnalyzer</td>
+            <td className={"border border-2 p-2 py-4 "}>News Analyzer</td>
             <td className="border border-2 p-4 py-4 ">
               <p className="py-3">
-                lets you analyze positivity and negativity of published new of a
-                specific new publisher.
+                - lets you analyze positivity and negativity of published new of
+                a specific new publisher.
               </p>
               <p className="py-3">
-                Used Beautifulsoup to scrap news and CSV to store data.
+                - Used Beautifulsoup to scrap news and CSV to store data.
               </p>
               <p className="py-3 ">
                 Tech stack: Python, Pandas, Beautifulsoup, Vader sentiment
@@ -58,20 +86,21 @@ export default function Projects() {
               <a
                 href="https://github.com/AshwinSomi/News-Analyzer"
                 className="underline py-3 font_tiny"
+                target="blank"
               >
                 github
               </a>
             </td>
           </tr>
           <tr>
-            <td className={"border border-2 p-4 py-4 "}>NewsAnalyzer</td>
+            <td className={"border border-2 p-2 py-4 "}>Path Visualizer</td>
             <td className="border border-2 p-4 py-4 ">
               <p className="py-3">
-                A path visualizer game that uses A* path finding algorithm to
+                - A path visualizer game that uses A* path finding algorithm to
                 find shortest route.
               </p>
               <p className="py-3">
-                Used Pygame to create dashboard for the game
+                - Used Pygame to create dashboard for the game
               </p>
               <p className="py-3 ">Tech stack: Python, Pygame</p>
             </td>
